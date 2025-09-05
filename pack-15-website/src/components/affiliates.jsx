824ba39cab8af2e457ecf4c18f2fb3f706306a34
Rob <bnglcat@hotmail.com>
2025-09-05 14:31:37 -0500

    Updated links and names

--- pack-15-website/src/components/affiliates.jsx
+++ pack-15-website/src/components/affiliates.jsx
@@ -1,61 +1,60 @@
 const Affiliates = () => {
   return (
     <>
-      <div className='row'>
-        <div className='subcontent'>
+      <div className="row">
+        <div className="subcontent">
           Our sponsors and affiliates are so important and we accomplish a lot
           together. We wouldn't be able to do what we do without them. They help
           us create meaningful experiences for our scouts. We are proud of our
           sponsors and affiliates - THANK YOU!
         </div>
         <hr />
-        <div className='subcontenttitle'>Rotary Club of St. George</div>
-        <div className='subcontent'>
+        <div className="subcontenttitle">Rotary Club of St. George</div>
+        <div className="subcontent">
           All Cub Scout Packs are associated with a Chartering Organization that
           sponsors them as an extension of their organization's mission to
           serving the community. They serve as a chartering partner, owner, and
-          approves the selection of leaders. Pack 15 is excited to work with the{' '}
+          approves the selection of leaders. Pack 15 is excited to work with the{" "}
           <a
-            href='https://www.rotarystgeorge.org/'
-            target='_blank'
-            rel='noreferrer'
+            href="https://www.rotarystgeorge.org/"
+            target="_blank"
+            rel="noreferrer"
           >
-            Rotary Club of Southeast Baton Rouge
-          </a>{' '}
+            Rotary Club of St. George
+          </a>{" "}
           to enhance the communities we touch.
         </div>
         <hr />
-        <div className='subcontenttitle'>
+        <div className="subcontenttitle">
           St. Andrews United Methodist Church
         </div>
-        <div className='subcontent'>
-          <a href='https://www.saumcbr.com/' target='_blank' rel='noreferrer'>
+        <div className="subcontent">
+          <a href="https://www.saumcbr.com/" target="_blank" rel="noreferrer">
             St. Andrews
-          </a>{' '}
+          </a>{" "}
           is gracious enough to allow Pack 15 to use their Friendship Building
           to host our monthly Pack Meetings as well as some of our Pack events
           like the Pinewood Derby each year. We are very grateful to have them
           in support of our Pack.
         </div>
         <hr />
-        <div className='subcontenttitle'>Istrouma Area Council</div>
-        <div className='subcontent'>
-          The{' '}
-          <a href='https://www.iacbsa.org/' target='_blank' rel='noreferrer'>
+        <div className="subcontenttitle">Istrouma Area Council</div>
+        <div className="subcontent">
+          The{" "}
+          <a href="https://www.istroumascouting.org/" target="_blank" rel="noreferrer">
             Istrouma Area Council (IAC)
-          </a>{' '}
+          </a>{" "}
           serves 13 parishes in Louisiana and Wilkinson County, Mississippi.
           They provide a quality Scouting program to co-ed youth with adult
           volunteers supporting Cub Scout Packs, Scouts BSA Troops, Venturing
           Crews, Sea Scouts, and Exploring Posts. Pack 15 is a member of the
-          Sewell-Eagle District of the Council.
+          Central District of the Council.
           <br />
           <br />
-          It is the mission of the Istrouma Area Council, Boys Scouts of
-          America, to build character, citizenship and total fitness while
-          preparing young people to make ethical and moral choices over the
-          course of their lifetimes by instilling in them the values of the
-          Scout Oath and Law.
+          It is the mission of the Istrouma Area Council, Scouting America, to
+          build character, citizenship and total fitness while preparing young
+          people to make ethical and moral choices over the course of their
+          lifetimes by instilling in them the values of the Scout Oath and Law.
         </div>
       </div>
     </>
